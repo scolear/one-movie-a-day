3955bdbd195c50a51ac9a50f5d5ebd4608ad87f7
scolear <bobtera@gmail.com>
2023-01-13 23:27:56 +0100

    chore: move sensitive data to .env

--- instagrapi-script.py
+++ instagrapi-script.py
@@ -1,6 +1,9 @@
-from instagrapi import Client
+from dotenv import load_dotenv
+load_dotenv()
 import os
 import json
+from instagrapi import Client
+
 
 if 'insta-session.json' in os.listdir():
     with open('insta-session.json', 'r') as f:
@@ -10,9 +13,9 @@ else:
     cl_session =  {}
 
 cl = Client(cl_session)
-cl.login('rhotauscolear', 'rS5D9?Tch?SFtgFJ')
+cl.login(os.environ["INSTAGRAM_USERNAME"], os.environ["INSTAGRAM_PASSWORD"])
 
 with open('insta-session.json', 'w') as f:
     json.dump(cl.get_settings(), f)
 
-cl.album_download(2724792305325435269, folder='./data/media')
+# cl.album_download(2724792305325435269, folder='./data/media')
