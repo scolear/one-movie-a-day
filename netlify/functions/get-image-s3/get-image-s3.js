4546584ea4dbf4821bbabac35afc66f5b475eac7
scolear <bobtera@gmail.com>
2023-01-19 00:36:01 +0100

    fix: env var

--- netlify/functions/get-image-s3/get-image-s3.js
+++ netlify/functions/get-image-s3/get-image-s3.js
@@ -1,8 +1,15 @@
 const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
-const client = new S3Client({ region: "eu-west-3" });
+const client = new S3Client({
+  region: "eu-west-3",
+  credentials: {
+    accessKeyId: process.env.S3_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_AWS_SECRET_ACCESS_KEY
+  }
+});
 
 const handler = async (event) => {
   try {
+
     const key = event.queryStringParameters.key;
     const response = await client.send(new GetObjectCommand({Bucket: 'onemovieaday', Key: key}));
     const buff = Buffer.from(await response.Body.transformToByteArray());
