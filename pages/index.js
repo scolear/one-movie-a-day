c18289f65528f8f2f8d780bf7165fa0e8c8f1bd4
scolear <bobtera@gmail.com>
2023-03-26 13:18:08 +0200

    more regex

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,36 @@
-import Head from 'next/head'
-import { useState, useEffect } from 'react';
-import { Center,
-  Container,
-  Stack,
-  Space,
-  Pagination
-} from '@mantine/core';
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-import { CarouselCard } from '@components/CardCarousel'
-import dataLoaderService from 'services/data-loader.service'
-import Controls from '@components/Controls';
+import Head from "next/head";
+import { useState, useEffect } from "react";
+import { Center, Container, Stack, Space, Pagination } from "@mantine/core";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+import { CarouselCard } from "@components/CardCarousel";
+import dataLoaderService from "services/data-loader.service";
+import Controls from "@components/Controls";
 
 const dataLoaderSvc = dataLoaderService();
 
 export default function Home() {
   const [data, setData] = useState(dataLoaderSvc.initialData);
   const [displayBatchSize, setDisplayBatchSize] = useState(5);
-  const [pagesNo, setPagesNo] = useState(Math.ceil(data.length / displayBatchSize));
+  const [pagesNo, setPagesNo] = useState(
+    Math.ceil(data.length / displayBatchSize)
+  );
   const [activePage, setPage] = useState(1);
   const [startIndex, setStartIndex] = useState(0);
   const [dataSlice, setDataSlice] = useState(data.slice(0, displayBatchSize));
 
   useEffect(() => {
     setStartIndex((activePage - 1) * displayBatchSize);
-  }, [activePage])
+  }, [activePage]);
 
   useEffect(() => {
-    setDataSlice(data.slice(startIndex, startIndex + displayBatchSize))
-  }, [startIndex])
+    setDataSlice(data.slice(startIndex, startIndex + displayBatchSize));
+  }, [startIndex]);
 
   useEffect(() => {
-    console.log('Setting new data');
-    setDataSlice(data.slice(startIndex, startIndex + displayBatchSize))
-  }, [data])
+    console.log("Setting new data");
+    setDataSlice(data.slice(startIndex, startIndex + displayBatchSize));
+  }, [data]);
 
   function orderByDate() {
     const newData = [...data];
@@ -54,31 +51,57 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Center>
-        <Header title={'#onemovieaday'}/>
+        <Header title={"#onemovieaday"} />
       </Center>
       <Center>
         <Controls byDate={orderByDate} byRating={orderByRating}></Controls>
       </Center>
       <Space h="sm" />
       <Center>
-        <Pagination page={activePage} onChange={setPage} total={pagesNo} siblings={1} color="red" size="sm" radius="md" withEdges/>
+        <Pagination
+          page={activePage}
+          onChange={setPage}
+          total={pagesNo}
+          siblings={0}
+          boundaries={1}
+          color="red"
+          size="sm"
+          radius="md"
+          withEdges
+        />
       </Center>
       <Space h="md" />
       <main>
         <Container size={600}>
           <Stack align="center">
-            {dataSlice.map(props => (
-              <CarouselCard key={props.key} pk={props.key} title={props.movie_title} rating={props.rating} text={props.comment} link={props.link} />
+            {dataSlice.map((props) => (
+              <CarouselCard
+                key={props.key}
+                pk={props.key}
+                title={props.movie_title}
+                rating={props.rating}
+                text={props.comment}
+                link={props.link}
+              />
             ))}
           </Stack>
         </Container>
       </main>
       <Space h="md" />
       <Center>
-        <Pagination page={activePage} onChange={setPage} total={pagesNo} siblings={1} color="red" size="sm" radius="md" withEdges/>
+        <Pagination
+          page={activePage}
+          onChange={setPage}
+          total={pagesNo}
+          siblings={1}
+          color="red"
+          size="sm"
+          radius="md"
+          withEdges
+        />
       </Center>
       <Space h="md" />
       <Footer />
     </div>
-  )
+  );
 }
--- services/data-loader.service.js
+++ services/data-loader.service.js
@@ -1,57 +1,100 @@
-import data from '../data/data.json';
+import data from "../data/data.json";
 
 const MEDIA_TYPES = {
   Photo: 1,
   Video: 2,
-  Album: 8
-}
+  Album: 8,
+};
 
 function spliceCaption(caption) {
-  const regex = /^(.+)\s+\(?(\d{4})?\)?\s*➡️\s*(\d+(\,?\.?\d+)?)\/10\s*(.*)$/s;
-  const match = caption.match(regex);
-  //TODO: we could do an if-else here instead to check for a modified regex which might capture more stuff (or create an ever better regex)
+  let title, date, rating, comment;
+
+  const regexPatterns = [
+    {
+      regex: /^(.+)\s+\(?(\d{4})?\)?\s*➡️\s*(\d+(\,?\.?\d+)?)\/10\s*(.*)$/s,
+      extract: (match) => {
+        [, title, date, rating, , comment] = match;
+      },
+    },
+    {
+      regex:
+        /([\w\s(),]+)\s+((?:\d(?:[.,]\d)?|10))\/10\s*([\s\S]*?)(?=(?:\d(?:[.,]\d)?|10)\/10|$)/,
+      extract: (match) => {
+        [, title, rating, comment] = match;
+      },
+    },
+    {
+      regex: /(.+)\s+((?:\d(?:[.,]\d)?|10))\/10\s*(.*)/,
+      extract: (match) => {
+        [, title, rating, comment] = match;
+      },
+    },
+  ];
+
+  let match;
+  for (const pattern of regexPatterns) {
+    match = caption.match(pattern.regex);
+    if (match) {
+      pattern.extract(match);
+      break;
+    }
+  }
+
   if (!match) {
     console.log(caption);
     return null;
   }
-  const [, title, date, rating, , comment] = match;
+
   return {
-    title: title + (date ? `${date}` : ''),
-    rating: { text: rating , value: parseFloat(rating) },
-    comments: comment
-  }
+    title: title + (date ? ` ${date}` : ""),
+    rating: { text: rating, value: parseFloat(rating) },
+    comments: comment,
+  };
 }
 
-// Removing duplicates, filtering for user, removing non-standard-compliant formatted ones
+// Removing duplicates, filtering for user
+// TODO: we could safely remove this function, if the data is already filtered
 function filterData(_data) {
-  const ids = _data.map(o => o.id);
-  return _data
-    .filter((post, index) => !ids.includes(post.id, index + 1))
-    .filter(post => post.user.username == "bogiaranyi")
-    // .filter(post => post.caption_text.includes('\u27a1\ufe0f'));
+  const ids = _data.map((o) => o.id);
+  return _data.filter(
+    (post, index) =>
+      !ids.includes(post.id, index + 1) && post.user.username == "bogiaranyi"
+  );
+}
+
+function processPost(post) {
+  const parsedCaption = spliceCaption(post.caption_text);
+  if (!parsedCaption) {
+    // console.log(`https://www.instagram.com/p/${post.code}/`, post.caption_text);
+    return;
+  }
+  return {
+    key: post.pk,
+    date: post.taken_at,
+    link: `https://www.instagram.com/p/${post.code}/`,
+    movie_title: parsedCaption.title,
+    rating: parsedCaption.rating.text,
+    ratingValue: parsedCaption.rating.value,
+    comment: parsedCaption.comments,
+    resourceKeys:
+      post.media_type == MEDIA_TYPES.Photo
+        ? [post.pk]
+        : post.resources.map((resource) => resource.pk),
+  };
 }
 
 const filteredData = filterData(data);
 
-const formattedData = filteredData.map(post => {
-  const parsedCaption = spliceCaption(post.caption_text);
-  if (!parsedCaption) {
-    console.log(`https://www.instagram.com/p/${post.code}/`, post.caption_text)
-    return;}
-  return {
-    "key": post.pk,
-    "date": post.taken_at,
-    "link": `https://www.instagram.com/p/${post.code}/`,
-    "movie_title": parsedCaption.title,
-    "rating": parsedCaption.rating.text,
-    "ratingValue": parsedCaption.rating.value,
-    "comment": parsedCaption.comments,
-    "resourceKeys": post.resources.map(resource => resource.pk)
+const formattedData = filteredData.reduce((acc, post) => {
+  const processedPost = processPost(post);
+  if (processedPost?.key) {
+    acc.push(processedPost);
   }
-}).filter(fd => fd?.key); // TODO: This is weird
+  return acc;
+}, []);
 
 export default () => {
   return {
-    initialData: formattedData
+    initialData: formattedData,
   };
-}
+};
